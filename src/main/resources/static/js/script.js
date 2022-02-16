d35b899d7c7f73e501a6ee50734fb7754085197f
GregorSomething <gregor.suurvarik@gmail.com>
2022-02-16 21:05:12 +0200

    Update Frontend

--- src/main/resources/static/js/script.js
+++ src/main/resources/static/js/script.js
@@ -4,19 +4,21 @@
 const app = Vue.createApp({
     data() {
         return {
-            screen: 'register',
-            // Type: [{name: String, description: String, price: float, type: String, category: String, count: int, imageUrl: String}]
+            screen: 'home',
+            // Type: [{id: int, name: String, description: String, price: float, type: String, category: String, count: int, imgUrl: String}]
             products: [
-                {id: 0, name: "Retro Burger", description: "Human food", price: 3.99, type: "Burger", category: "Toit", count: 347, imageUrl: 'https://mamma.ee/arendus/wp-content/uploads/2020/11/Mamma-Retroburger-230g.jpg'},
-                {id: 1, name: "Kanapitsa Ananassiga", description: "Human food", price: 8.99, type: "Pitsa", category: "Toit", count: 10, imageUrl: 'https://superskypark.ee/wp-content/uploads/2020/01/kana-ananassi-pitsa.jpg'},
-                {id: 2, name: "Rafaello", description: "Human food", price: 6.99, type: "Kommid", category: "Toit", count: 1, imageUrl: 'https://balticfresh.com/image/cache/catalog/April%20products/ferrero-rafaello-15-pcs-800x800.jpg'},
-                {id: 0, name: "Retro Burger", description: "Human food", price: 3.99, type: "Burger", category: "Toit", count: 347, imageUrl: 'https://mamma.ee/arendus/wp-content/uploads/2020/11/Mamma-Retroburger-230g.jpg'},
-                {id: 1, name: "Kanapitsa Ananassiga", description: "Human food", price: 8.99, type: "Pitsa", category: "Toit", count: 10, imageUrl: 'https://superskypark.ee/wp-content/uploads/2020/01/kana-ananassi-pitsa.jpg'},
-                {id: 2, name: "Rafaello", description: "Human food", price: 6.99, type: "Kommid", category: "Toit", count: 1, imageUrl: 'https://balticfresh.com/image/cache/catalog/April%20products/ferrero-rafaello-15-pcs-800x800.jpg'},
-                {id: 0, name: "Retro Burger", description: "Human food", price: 3.99, type: "Burger", category: "Toit", count: 347, imageUrl: 'https://mamma.ee/arendus/wp-content/uploads/2020/11/Mamma-Retroburger-230g.jpg'},
-                {id: 1, name: "Kanapitsa Ananassiga", description: "Human food", price: 8.99, type: "Pitsa", category: "Toit", count: 10, imageUrl: 'https://superskypark.ee/wp-content/uploads/2020/01/kana-ananassi-pitsa.jpg'},
-                {id: 2, name: "Rafaello", description: "Human food", price: 6.99, type: "Kommid", category: "Toit", count: 1, imageUrl: 'https://balticfresh.com/image/cache/catalog/April%20products/ferrero-rafaello-15-pcs-800x800.jpg'},
-            ]
+                {id: 0, name: "Retro Burger", description: "Human food", price: 3.99, type: "Burger", category: "Toit", count: 347, imgUrl: 'https://mamma.ee/arendus/wp-content/uploads/2020/11/Mamma-Retroburger-230g.jpg'},
+                {id: 1, name: "Kanapitsa Ananassiga", description: "Human food", price: 8.99, type: "Pitsa", category: "Toit", count: 10, imgUrl: 'https://superskypark.ee/wp-content/uploads/2020/01/kana-ananassi-pitsa.jpg'},
+                {id: 2, name: "Rafaello", description: "Human food", price: 6.99, type: "Kommid", category: "Toit", count: 1, imgUrl: 'https://balticfresh.com/image/cache/catalog/April%20products/ferrero-rafaello-15-pcs-800x800.jpg'},
+                {id: 0, name: "Retro Burger", description: "Human food", price: 3.99, type: "Burger", category: "Toit", count: 347, imgUrl: 'https://mamma.ee/arendus/wp-content/uploads/2020/11/Mamma-Retroburger-230g.jpg'},
+                {id: 1, name: "Kanapitsa Ananassiga", description: "Human food", price: 8.99, type: "Pitsa", category: "Toit", count: 10, imgUrl: 'https://superskypark.ee/wp-content/uploads/2020/01/kana-ananassi-pitsa.jpg'},
+                {id: 2, name: "Rafaello", description: "Human food", price: 6.99, type: "Kommid", category: "Toit", count: 1, imgUrl: 'https://balticfresh.com/image/cache/catalog/April%20products/ferrero-rafaello-15-pcs-800x800.jpg'},
+                {id: 0, name: "Retro Burger", description: "Human food", price: 3.99, type: "Burger", category: "Toit", count: 347, imgUrl: 'https://mamma.ee/arendus/wp-content/uploads/2020/11/Mamma-Retroburger-230g.jpg'},
+                {id: 1, name: "Kanapitsa Ananassiga", description: "Human food", price: 8.99, type: "Pitsa", category: "Toit", count: 10, imgUrl: 'https://superskypark.ee/wp-content/uploads/2020/01/kana-ananassi-pitsa.jpg'},
+                {id: 2, name: "Rafaello", description: "Human food", price: 6.99, type: "Kommid", category: "Toit", count: 1, imgUrl: 'https://balticfresh.com/image/cache/catalog/April%20products/ferrero-rafaello-15-pcs-800x800.jpg'},
+            ],
+            // Type: [{cartCount: int, item: {id: int, name: String, description: String, price: float, type: String, category: String, count: int, imgUrl: String}}]
+            cart: []
         };
     },
     computed: {
@@ -31,7 +33,7 @@ const app = Vue.createApp({
             return /^[a-zA-Z0-9_]{5,100}$/.test(username);
         },
         UTIL_checkPassword(password) {
-            return /^[a-zA-Z0-9_'!"#%&/()=+\-*]{7,100}$/.test(password);
+            return /^[a-zA-Z0-9_'!"#%&/()=+\-*]{4,100}$/.test(password);
         },
         UTIL_checkDisplayName(displayName) {
             return /^[a-zA-Z0-9_ ]{5,100}$/.test(displayName);
@@ -41,7 +43,7 @@ const app = Vue.createApp({
             ### UI FUNCTIONS ###
             #################### */
 
-        UI_onSubmitLogin() {
+        UI_onSubmitLogin(event) {
             console.log('username:', event.target.elements.username.value);
             console.log('password:', event.target.elements.password.value);
             console.log('remember-me:', event.target.elements["remember-me"].checked);
@@ -51,7 +53,7 @@ const app = Vue.createApp({
             }
         },
 
-        UI_onSubmitRegister() {
+        UI_onSubmitRegister(event) {
             console.log('username:', event.target.elements.username.value);
             console.log('displayName:', event.target.elements.displayName.value);
             console.log('password:', event.target.elements.password.value);
@@ -66,7 +68,7 @@ const app = Vue.createApp({
             ##################### */
 
         /**
-            @returns [{name: String, description: String, price: float, type: String, category: String, count: int, imageUrl: String}]
+            @returns [{id: int, name: String, description: String, price: float, type: String, category: String, count: int, imgUrl: String}]
             Retrieves all products from the server.
         */
         async API_allProducts() {
@@ -77,7 +79,7 @@ const app = Vue.createApp({
             @param name         Products with that name.
             @param type         Products of that type.
             @param category     Products in that category.
-            @returns [{name: String, description: String, price: float, type: String, category: String, count: int, imageUrl: String}]
+            @returns [{id: int, name: String, description: String, price: float, type: String, category: String, count: int, imgUrl: String}]
             Searches for products.
         */
         async API_products(name, type, category) {
@@ -99,13 +101,52 @@ const app = Vue.createApp({
         /**
             @returns {
                 price: float,
-                items: [{name: String, description: String, price: float, type: String, category: String, count: int, imageUrl: String}]
+                items: [{item: {id: int, name: String, description: String, price: float, type: String, category: String, count: int, imgUrl: String}, cartCount: int}]
             }
         */
         async API_getCart() {
             return fetch("api/cart/get")
                 .then(res => res.json());
         },
+        /**
+            @returns {
+                username: String, clientPass: null, roles: ["ADMIN"], sRoles: ["ADMIN"], imageUrl: URL, displayName: String, enabled: boolean, password: null, authorities: null, credentialsNonExpired: boolean, accountNonExpired: boolean, accountNonLocked: boolean
+            }
+         */
+        async API_getUser() {
+            return fetch("api/user/get")
+                .then(res => res.json());
+        },
+
+
+
+        findCartProduct(id) {
+            return this.cart.find(cartProduct => cartProduct.item && cartProduct.item.id == id);
+        },
+        decrementCount(id) {
+            let cartProduct = this.findCartProduct(id);
+            if(cartProduct && cartProduct.cartCount > 0) {
+                cartProduct.cartCount--;
+                this.API_addToCart(id, cartProduct.cartCount);
+            }
+        },
+        incrementCount(id) {
+            let cartProduct = this.findCartProduct(id);
+            if(!cartProduct) {
+                this.cart.push({id, cartCount: 1});
+            } else {
+                cartProduct.cartCount++;
+            }
+            this.API_addToCart(id, cartProduct.cartCount);
+        },
+        async getCart() {
+            this.cart = (await this.API_getCart()).items;
+        }
+    },
+    mounted() {
+        this.API_allProducts().then(products => {
+            if(products) this.products = products;
+        });
     }
 }); 
 
